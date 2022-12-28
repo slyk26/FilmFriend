cbe7262c54f4c3d82bedfd909131a5ec912f454b
slyk26 <->
2022-12-28 20:55:40 +0100

    my first slash command

---
+++ src/handlers/interaction_create.rs
@@ -0,0 +1,34 @@
+use serenity::async_trait;
+
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+
+use serenity::prelude::*;
+use crate::commands;
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command.data.name);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+}
