2b55aa905bc3996f2ec74a8a0ddf725ca8e71a44
slyk26 <->
2023-01-07 17:02:53 +0100

    restrict commands only to a server

--- src/event_handler/events/interaction_create.rs
+++ src/event_handler/events/interaction_create.rs
@@ -1,23 +1,38 @@
-use serenity::model::application::interaction::{Interaction};
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 
 use serenity::prelude::*;
 use crate::COMMANDS;
 use crate::event_handler::events::interactions::*;
 
 pub async fn call(ctx: &Context, interaction: &Interaction) {
-    match interaction {
+    if interaction.guild_locale().is_some() {
+        match interaction {
 
-        // regular response (text) => returns the result of the called SlashCommand
-        Interaction::ApplicationCommand(interaction) => {
-            let cmd = COMMANDS.get(interaction.data.name.as_str()).expect("No Command found in command map");
-            application_command::call(&ctx, &interaction, &cmd).await;
+            // regular response (text) => returns the result of the called SlashCommand
+            Interaction::ApplicationCommand(interaction) => {
+                let cmd = COMMANDS.get(interaction.data.name.as_str()).expect("No Command found in command map");
+                application_command::call(&ctx, &interaction, &cmd).await;
+            }
+
+            // response after you call a SlashCommand with .kind(InteractionResponseType::Modal)
+            Interaction::ModalSubmit(interaction) => {
+                modal_submit::call(&ctx, &interaction).await;
+            }
+
+            _ => { println!("unknown interaction type") }
         }
+    } else {
+        match interaction {
+            Interaction::ApplicationCommand(interaction) => {
+                let _ = interaction
+                    .create_interaction_response(&ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| message.content("The bot only works in a Server"))
+                    }).await;
+            }
 
-        // response after you call a SlashCommand with .kind(InteractionResponseType::Modal)
-        Interaction::ModalSubmit(interaction) => {
-            modal_submit::call(&ctx, &interaction).await;
+            _ => { println!("unknown interaction type") }
         }
-
-        _ => { println!("unknown interaction type") }
-    }
+     }
 }
